991f23c7e9981e7e2c63ab77f761f22cdc9f36fe
DmitrySergienko <dmitrisergienko@gmail.com>
2023-05-25 12:14:05 +0400

    empty login and home screen before sqlite integ

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+
 
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 
 const Stack = createStackNavigator();
 
-
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-      initialRouteName='Login'
-      >
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={Home} />
 
         <Stack.Screen name="Login" component={Login} />
@@ -23,16 +20,5 @@ function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#ffff32',
-  },
-});
 
 export default App;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,22 +1,49 @@
-import React from 'react';
-import {View,StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
-export default function Login(){
-    return(
-        <View
-        style ={styles.body}>
 
-        </View>
-    )
 
+export default function Login({navigation}) {
+  const [name, setName] = useState('');
+
+  //in case no input Alert, or navigate to Login
+  const setData = () => {
+    if (name.length == 0) {
+      Alert.alert('Warning', 'No value entrered');
+    } else {
+      navigation.navigate('Home')
+    }
+  };
+
+  return (
+   
+      <View style={styles.body}>
+      <Text style={styles.text}>Logo screen</Text>
+      <TextInput style={styles.input} onChangeText={value => setName(value)} />
+      <View style={{marginTop: 40}}>
+        <Button style={styles.input} onPress={setData} title="Button" />
+      </View>
+    </View>
+  
+    
+  );
 }
 
 const styles = StyleSheet.create({
-  body:{  flex:1,
-    alignItems:'center',
-    backgroundColor: '#0080ff'
-  }
-})
-
-
-
+  body: {flex: 1, alignItems: 'center', backgroundColor: '#0080ff'},
+  text: {
+    fontSize: 30,
+    color: '#ffffff',
+    marginTop: 20,
+  },
+  input: {
+    width: 300,
+    borderWidth: 1,
+    borderColor: '#ffffff',
+    borderRadius: 10,
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 130,
+    backgroundColor: '#ffffff',
+  },
+});
