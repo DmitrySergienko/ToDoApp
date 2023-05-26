e3654912412608cc5e82505bfc844b843b4714fc
DmitrySergienko <dmitrisergienko@gmail.com>
2023-05-26 08:16:30 +0400

    SQLite implemented

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text,ScrollView } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
 
-//create db ========================
+//create SQLdb ========================
 const db = SQLite.openDatabase(
   {
     name: 'MainDB',
     location: 'default',
   },
-  () => {},
+  () => { },
   error => {
     console.log(error);
   },
@@ -23,10 +23,12 @@ export default function Home() {
   const getUsers = () => {
     try {
       db.transaction(tx => {
-        tx.executeSql('SELECT Name, Age FROM Users', [], (tx, results) => {
+         tx.executeSql('SELECT Name, Age FROM Users', [], (tx, results) => {
           var len = results.rows.length;
           if (len > 0) {
+
             var usersArray = [];
+            
             for (let i = 0; i < len; i++) {
               var user = results.rows.item(i);
               usersArray.push(user);
@@ -46,21 +48,25 @@ export default function Home() {
   }, []);
 
   return (
-    
+
     <View style={styles.body}>
       {isLoading ? (
         <Text style={styles.text}>Loading...</Text>
       ) : (
         <ScrollView contentContainerStyle={styles.scrollContainer}>
+
           <Text style={styles.header}>Users:</Text>
+
           {users.map((user, index) => (
             <View key={index} style={styles.item}>
               <Text style={styles.text}>Name: {user.Name}</Text>
               <Text style={styles.text}>Age: {user.Age}</Text>
             </View>
           ))}
+
           {users.length === 0 && <Text style={styles.text}>No users found.</Text>}
-          </ScrollView>
+
+        </ScrollView>
       )}
     </View>
   );
